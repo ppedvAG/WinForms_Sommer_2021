c22ebb71db9d5fe7c8bce9a36ff6d0594ca1fd9c
André <AndreR@outlook.de>
2021-08-13 16:29:18 +0200

    Hallo Datenbank

---
+++ HalloDatenbank/HalloDatenbank/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace HalloDatenbank
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.Bisque;
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(774, 48);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Lade Employees";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 32;
+            this.listBox1.Location = new System.Drawing.Point(0, 48);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(774, 363);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.Format += new System.Windows.Forms.ListControlConvertEventHandler(this.listBox1_Format);
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(774, 411);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ HalloDatenbank/HalloDatenbank/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HalloDatenbank
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            conString = "Server=(localdb)\\mssqllocaldb;Database=Northwind;Trusted_Connection=true;";
+        }
+
+        string conString = "Server=.;Database=Northwind;Trusted_Connection=true;";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                con.Open();
+
+                using (SqlCommand cmd = new SqlCommand("SELECT count(*) FROM Employees"))
+                {
+                    cmd.Connection = con;
+
+                    object result = cmd.ExecuteScalar();
+                    if (result is int count)
+                        Text = $"{count} Employees in DB";
+                }
+
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Employees"))
+                {
+                    cmd.Connection = con;
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read()) //zeileweise aus der DB die Daten holen
+                    {
+                        Employee emp = new Employee()
+                        {
+                            Id = reader.GetInt32(0),
+                            Nachname = reader.GetString(1),
+                            Vorname = reader.GetString(2),
+                            GebDatum = reader.GetDateTime(5)
+                        };
+
+                        //listBox1.Items.Add($"{emp.Vorname} {emp.Nachname} {emp.GebDatum:d}"); // selected Item == string
+                        listBox1.Items.Add(emp); // selected Item == Employee
+                    }
+                }
+
+            }  //con.Dispose(); --> con.Close();
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem is Employee emp)
+            {
+                using (SqlConnection con = new SqlConnection(conString))
+                {
+                    con.Open();
+
+                    //using (SqlCommand cmd = new SqlCommand($"UPDATE Employees SET BirthDate={emp.GebDatum} WHERE EmployeeID={emp.Id}")) //bäh!
+                    //sql injection ';DELETE users;--
+                    using (SqlCommand cmd = new SqlCommand("UPDATE Employees SET BirthDate=@newBDate WHERE EmployeeID=@id"))
+                    {
+                        cmd.Connection = con;
+                        cmd.Parameters.AddWithValue("@newBDate", emp.GebDatum.AddYears(1));
+                        cmd.Parameters.AddWithValue("@id", emp.Id);
+
+                        int affectedRows = cmd.ExecuteNonQuery();
+                        if (affectedRows == 0)
+                            MessageBox.Show("Nicht wurde veränder");
+                        else if (affectedRows == 1)
+                            MessageBox.Show($"{emp.Vorname} wurde verjüngt");
+                        else
+                            MessageBox.Show("PANIK!");
+                    }
+                }
+            }
+        }
+
+        private void listBox1_Format(object sender, ListControlConvertEventArgs e)
+        {
+            if (e.ListItem is Employee emp)
+                e.Value = $"{emp.Vorname}  {emp.Nachname} {emp.GebDatum:d}";
+        }
+    }
+
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public DateTime GebDatum { get; set; }
+    }
+}
